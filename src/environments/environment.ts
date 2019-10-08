4a3118ab01f2d855241776a1aa8eb9d93e125aef
mur2c <37222855+mur2c@users.noreply.github.com>
2019-10-08 17:55:31 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyDeQlHEhF9wTEGg-QCRqkXTGT0Vrjl-DPc",
-    authDomain: "ionsolecom.firebaseapp.com",
-    databaseURL: "https://ionsolecom.firebaseio.com",
-    projectId: "ionsolecom",
-    storageBucket: "",
-    messagingSenderId: "52599332118",
-    appId: "1:52599332118:web:d4a0ad57ab2ade29"
-  }
+  production: false,
 };
 
 /*
