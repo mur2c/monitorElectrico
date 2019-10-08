029c3b9abd4b0049ab49610803818837d76f6206
mur2c <37222855+mur2c@users.noreply.github.com>
2019-10-08 17:56:06 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyDeQlHEhF9wTEGg-QCRqkXTGT0Vrjl-DPc",
-    authDomain: "ionsolecom.firebaseapp.com",
-    databaseURL: "https://ionsolecom.firebaseio.com",
-    projectId: "ionsolecom",
-    storageBucket: "",
-    messagingSenderId: "52599332118",
-    appId: "1:52599332118:web:d4a0ad57ab2ade29"
-  }
 };
